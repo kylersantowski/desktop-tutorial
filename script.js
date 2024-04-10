4487a4bb9c76676d46f70b312b1bd01f5e879539
Kyle Santowski <santowskikr@gmail.com>
2024-04-10 09:24:38 +0200

    Update and rename index.html to script.js

---
+++ script.js
@@ -0,0 +1,24 @@
+// File: script.js
+
+// Function to add two numbers
+function add(a, b) {
+    return a + b;
+}
+
+// Function to subtract two numbers
+function subtract(a, b) {
+    return a - b;
+}
+
+// Function to multiply two numbers
+function multiply(a, b) {
+    return a * b;
+}
+
+// Function to divide two numbers
+function divide(a, b) {
+    if (b === 0) {
+        throw new Error("Division by zero");
+    }
+    return a / b;
+}
